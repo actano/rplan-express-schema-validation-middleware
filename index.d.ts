43891dabaec79916f02562b62ad0b26a5398ec6b
Matthias Kronschnabl <matthias.kronschnabl@actano.de>
2020-06-02 18:16:00 +0200

    PA-4975: Add TypeScript declaration file

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+declare module "@rplan/express-schema-validation-middleware" {
+  import Express = require('express')
+
+  interface ParamsDictionary { [key: string]: string; }
+  type ParamsArray = string[];
+  type Params = ParamsDictionary | ParamsArray;
+
+  export interface OpenApiValidatorFactoryResult<P extends Params = ParamsDictionary, ResBody = any, ReqBody = any> {
+    getBodyValidationMiddleware: (path: string[]) => Express.RequestHandler<Params, ReqBody, ResBody>,
+    getParamsValidationMiddleware: (path: string[]) => Express.RequestHandler<Params, ReqBody, ResBody>,
+    getQueryValidationMiddleware: (path: string[]) => Express.RequestHandler<Params, ReqBody, ResBody>,
+    getHeaderValidationMiddleware: (path: string[]) => Express.RequestHandler<Params, ReqBody, ResBody>,
+    getResponseValidationEndPoint: (path: string[]) => {
+      validator: (httpCode: string) => {
+        validate: (res: Express.Response) => boolean,
+        errors: object[],
+      },
+    },
+  }
+
+  export function OpenApiValidator<P extends Params = ParamsDictionary, ResBody = any, ReqBody = any>(
+    filePath: string,
+    options?: { [key: string]: any },
+  ): OpenApiValidatorFactoryResult<Params, ReqBody, ResBody>
+}
